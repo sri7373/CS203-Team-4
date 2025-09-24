77a7251f5173b699eeffeb9dfff80fbe2b76e9ac
zegai0603 <87908832+zegai0603@users.noreply.github.com>
2025-09-24 15:23:45 +0800

    Set up base of our app

---
+++ backend/src/main/java/com/smu/tariff/TariffApplication.java
@@ -0,0 +1,11 @@
+package com.smu.tariff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TariffApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TariffApplication.class, args);
+    }
+}
