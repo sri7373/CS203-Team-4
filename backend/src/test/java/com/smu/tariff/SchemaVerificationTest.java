67c7a1e66aad4b2ed23a2d159763c89f70913857
Tew Zhe Khai <87908832+zegai0603@users.noreply.github.com>
2025-11-10 16:23:31 +0800

    Fix CI tests: Add test configuration and ActiveProfiles annotation

--- backend/src/test/java/com/smu/tariff/SchemaVerificationTest.java
+++ backend/src/test/java/com/smu/tariff/SchemaVerificationTest.java
@@ -12,8 +12,10 @@ import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SchemaVerificationTest {
 
     @Autowired
